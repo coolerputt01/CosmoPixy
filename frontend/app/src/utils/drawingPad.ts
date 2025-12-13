42d2c811381ee1d4d3f398f1b780e989ab0d0c5b
coolerputt <olumideadekolu9@gmail.com>
2025-12-13 15:14:30 +0000

    Had issues testing due to codespace reverse proxy issue

--- frontend/app/src/utils/drawingPad.ts
+++ frontend/app/src/utils/drawingPad.ts
@@ -2,25 +2,47 @@ let canvas : HTMLCanvasElement;
 let ctx : CanvasRenderingContext2D;
 
 const tileSize : number =   16;
+let isDrawingClickAndDrag : boolean = false;
 
 function initPad(){
     canvas = document.querySelector('canvas') as HTMLCanvasElement;
     ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-    canvas.width = 48 * tileSize;
+    canvas.width = 32 * tileSize;
     canvas.height = 32 * tileSize;
     ctx.fillStyle = "#000000";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     canvas.addEventListener("click",handleClick);
+    canvas.addEventListener("touchstart",()=>{
+        isDrawingClickAndDrag = true;
+    });
+    canvas.addEventListener("touchmove",(e: TouchEvent)=>{
+        if(isDrawingClickAndDrag){
+            handleClick(e);
+        }
+    });
+    canvas.addEventListener("touchend",()=>{
+        isDrawingClickAndDrag = false;
+    });
 }
 
-function handleClick( e : MouseEvent){
-        let canvasRect = canvas.getBoundingClientRect();
-        let clientX = ((e.clientX - canvasRect.left)/ tileSize) * tileSize;
-        let clientY = ((e.clientY - canvasRect.top)/tileSize) * tileSize;
+function handleClick( e : MouseEvent | TouchEvent){
+    let canvasRect = canvas.getBoundingClientRect();
+    if(e instanceof MouseEvent){
+        let clientX = Math.floor((e.clientX - canvasRect.left) / tileSize) * tileSize;
+		let clientY = Math.floor((e.clientY - canvasRect.top) / tileSize) * tileSize;
+
 
         ctx.fillStyle = 'red';
         ctx.fillRect(clientX, clientY, tileSize, tileSize);
+    }else{
+        let t = e.touches[0];
+        let clientX = Math.floor((t!.clientX - canvasRect.left)/tileSize) * tileSize;
+		let clientY = Math.floor((t!.clientY - canvasRect.right)/tileSize) * tileSize;
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(clientX,clientY,tileSize,tileSize);
+    }
 }
 
 function loop(){
--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -7,6 +7,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+  	host: "0.0.0.0",
+  	port: 5173,
+    strictPort: true,
+    open: false
+	},
   plugins: [
     vue(),
     vueDevTools(),
